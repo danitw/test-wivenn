c62092af97b9e1c4d10997078c076fcb7f92fe7e
danitw <danidevpy@gmail.com>
2022-12-06 23:31:48 -0300

    create users migration and adding laravel sanctum

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,13 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            //$table->string('name');
+            $table->string('username');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('password', $max = 32, $min = 8);
+            $table->boolean('isAdmin')->default(false);
+            $table->timestamp('entryDate');
+            //$table->timestamp('email_verified_at')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
