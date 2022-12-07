9f54ab7d8a00b537918f1feb37b49b0367cbde61
danitw <danidevpy@gmail.com>
2022-12-07 00:26:34 -0300

    make relationship table reportsColaborators

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class User extends Authenticatable
 {
@@ -43,4 +44,13 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+     /**
+     * The reports that belong to the user.
+     * @return Illuminate\Database\Eloquent\Relations\BelongsToMany
+     */
+    public function reports():BelongsToMany
+    {
+        return $this->belongsToMany(Report::class, 'reportsColaborators', 'userId', 'reportId');
+    }
 }
---
+++ database/migrations/2022_12_07_031208_create_report_colaborators_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reportsColaborators', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('userId')->references('id')->on('users');
+            $table->foreignId('reportId')->references('id')->on('reports');
+            //$table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reportsColaborators');
+    }
+};
