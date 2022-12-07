7d220061955ae252748bc0d9c840448000f4d686
danitw <danidevpy@gmail.com>
2022-12-07 00:08:41 -0300

    create reports migration and model

---
+++ app/Models/Report.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasOne;
+
+class Report extends Model
+{
+  use HasFactory;
+
+  protected $fillable = [
+    'title',
+    'summary',
+    'creatorID',
+    'fileInServer'
+  ];
+
+    /**
+     * Get the user associated with the report.
+     *
+     * @return Illuminate\Database\Eloquent\Relations\HasOne
+     */
+    public function user():HasOne {
+        return $this->hasOne(User::class, 'creatorID');
+    }
+}
